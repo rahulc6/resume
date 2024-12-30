d6b6f9069ecdb9031da052afd4828af36353905f
RAHUL CR <65558128+rahulc6@users.noreply.github.com>
2024-12-30 21:06:34 +0530

    Initial Commit

---
+++ tools.R
@@ -0,0 +1,61 @@
+R 
+- libraries 
+-- dplyr
+-- tidyr
+-- ggplot2
+-- etc
+
+Regrssion Modelling
+- Linear Regression
+- Mixed Effect model
+- Partial Prediction regression model
+- 
+
+
+Machine Learning
+- Xgboost
+- Gradient boost
+- Nearest Neighbourhood methods
+- shap analysis
+
+version control and collaboration
+- bitbucket
+- gitea
+- jira
+- Atlassian confluence
+- slack
+- Notion
+
+Microsoft Office  
+- Advanced excel
+-- pivoting 
+-- macros
+- ppt
+- word
+
+SQL 
+- basic data wrangling and analysis
+-
+  
+rmarkdown
+- html
+- pdf
+
+Quarto
+-  PPT
+- html
+- pdf
+
+R-shiny
+dashboards
+
+python
+- streamlitdashboard
+
+docker
+- image creation 
+- conatiner deployment
+
+
+
+
